f62257b3955cd61d41354eddf99d4803db373f53
Julien DIDIER <julien@didier.io>
2015-01-27 11:59:21 +0100

    using routing component

--- web/index.php
+++ web/index.php
@@ -4,23 +4,27 @@ require_once __DIR__.'/../src/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
- 
+use Symfony\Component\Routing;
+
+$routes = new Routing\RouteCollection();
+$routes->add('hello', new Routing\Route('/hello/{name}', array('name' => 'World')));
+$routes->add('bye', new Routing\Route('/bye'));
+
 $request = Request::createFromGlobals();
-$response = new Response();
- 
-$map = array(
-    '/hello' => 'hello',
-    '/bye'   => 'bye',
-);
- 
-$path = $request->getPathInfo();
-if (isset($map[$path])) {
+
+$context = new Routing\RequestContext();
+$context->fromRequest($request);
+$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
+
+try {
+    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
     ob_start();
-    extract($request->query->all(), EXTR_SKIP);
-    include sprintf(__DIR__.'/../src/pages/%s.php', $map[$path]);
+    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
     $response = new Response(ob_get_clean());
-} else {
+} catch (Routing\Exception\ResourceNotFoundException $e) {
     $response = new Response('Not Found', 404);
+} catch (Exception $e) {
+    $response = new Response('An error occurred', 500);
 }
 
 $response->send();
