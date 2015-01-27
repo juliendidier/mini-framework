35cbd8a355356d472820233e0e2a126c17f959a2
Julien DIDIER <julien@didier.io>
2015-01-27 11:20:55 +0100

    bootstrap application

---
+++ web/index.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once __DIR__.'/../src/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+ 
+$request = Request::createFromGlobals();
+$response = new Response();
+ 
+$map = array(
+    '/hello' => 'hello',
+    '/bye'   => 'bye',
+);
+ 
+$path = $request->getPathInfo();
+if (isset($map[$path])) {
+    ob_start();
+    extract($request->query->all(), EXTR_SKIP);
+    include sprintf(__DIR__.'/../src/pages/%s.php', $map[$path]);
+    $response = new Response(ob_get_clean());
+} else {
+    $response = new Response('Not Found', 404);
+}
+
+$response->send();
